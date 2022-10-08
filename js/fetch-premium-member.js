e133920e84be9c44fa19f89abbdbe7e324f3a187
webozza <webozza@gmail.com>
2022-10-08 22:34:48 +0800

    backtrack

--- js/fetch-premium-member.js
+++ js/fetch-premium-member.js
@@ -18,8 +18,7 @@ async function renderUsers() {
   let users = await getUsers();
   let html = "";
   users.forEach((user) => {
-    if (user.website !== window.location.pathname) {
-      let htmlSegment = `<div class="busiess_div_premium">
+    let htmlSegment = `<div class="busiess_div_premium">
   
 			  <div class="business_details_div_parent">
 				  <div class="business_details_div_flex">
@@ -42,8 +41,8 @@ async function renderUsers() {
 					  <div class="business_contact_info">
 						  <p class="business_phone"></p>
 						  <p class="business_location">${user.clients_zip_code} ${
-        user.business_name
-      }</p>
+      user.business_name
+    }</p>
 					  </div>
 				  </div>
 				  <div class="business_website_btn_div">
@@ -56,8 +55,7 @@ async function renderUsers() {
   
 		  </div>`;
 
-      html += htmlSegment;
-    }
+    html += htmlSegment;
   });
 
   let container = document.querySelector(".premium-members-container");
