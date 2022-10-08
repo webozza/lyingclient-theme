fbc402585884513e76c74f72a68aa48d97470ade
webozza <webozza@gmail.com>
2022-10-08 17:23:07 +0800

    timeout set for catching a url slug

--- js/client-experience-search.js
+++ js/client-experience-search.js
@@ -1,9 +1,11 @@
-// ...checks for image src and slug in url
-if (
-  $("#client-experience-details img").attr("src").indexOf("no.png") > -1 &&
-  window.location.href.indexOf("/?experience_slug") > -1
-) {
-  $(".attachment_div").hide();
-} else {
-  console.log("is false");
-}
+setTimeout(function () {
+  if (
+    // ...checks for image src and slug in url
+    $("#client-experience-details img").attr("src").indexOf("no.png") > -1 &&
+    window.location.href.indexOf("/?experience_slug") > -1
+  ) {
+    $(".attachment_div").hide();
+  } else {
+    console.log("is false");
+  }
+}, 1000);
