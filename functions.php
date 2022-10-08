f1356668f401e3e89c880cf0f78eab84e7fd5906
webozza <webozza@gmail.com>
2022-10-08 16:04:56 +0800

    updated file name

--- functions.php
+++ functions.php
@@ -8,6 +8,6 @@ function astra_child_style() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 	wp_enqueue_script( 'fetch-premium-member', get_stylesheet_directory_uri() . '/js/fetch-premium-member.js', array( 'jquery' ) );
-	wp_enqueue_script( 'client-exp-search', get_stylesheet_directory_uri() . '/js/client-exp-search.js', array( 'jquery' ) );
+	wp_enqueue_script( 'client-exp-search', get_stylesheet_directory_uri() . '/js/client-experience-search.js', array( 'jquery' ) );
 }
 add_action( 'wp_enqueue_scripts', 'astra_child_style' );
