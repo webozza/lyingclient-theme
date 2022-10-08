6dac6bd3afbd403a6b6c8c801fecba1a74ccf75e
webozza <webozza@gmail.com>
2022-10-08 17:41:01 +0800

    fix for trigger delay of script

--- js/client-experience-search.js
+++ js/client-experience-search.js
@@ -1,8 +1,7 @@
 setTimeout(function () {
   if (
-    // ...checks for image src and slug in url
-    $("#client-experience-details img").attr("src").indexOf("no.png") > -1 &&
-    window.location.href.indexOf("/?experience_slug") > -1
+    window.location.pathname.indexOf("client-experience") > -1 &&
+    $("#client-experience-details img").attr("src").indexOf("no.png") > -1
   ) {
     $(".attachment_div").hide();
   } else {
