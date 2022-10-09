18d9518c2d758b871118baed4fd6e2b1b63c5097
webozza <webozza@gmail.com>
2022-10-09 20:47:18 +0800

    dependencies added

--- functions.php
+++ functions.php
@@ -7,6 +7,6 @@
 function astra_child_style() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
-	// wp_enqueue_script( 'fetch-premium-members', get_stylesheet_directory_uri() . '/js/fetch-premium-members.js', array( 'jquery' ) );
+	wp_enqueue_script( 'premium-members-club', get_stylesheet_directory_uri() . '/js/premium-members-club.js', array( 'jquery' ) );
 }
 add_action( 'wp_enqueue_scripts', 'astra_child_style' );
--- partials/premium-members-search.php
+++ partials/premium-members-search.php
@@ -12,11 +12,9 @@
 <?php echo do_shortcode("[ihc-list-users num_of_entries ='10' entries_per_page ='100' order_by ='user_registered' order_type ='desc' filter_by_level ='1' levels_in ='3' user_fields ='city,businessnameump,businesscategoryump,zip,user_url' theme ='ihc-theme_8' color_scheme ='ee3733' columns ='5' align_center ='1' show_search ='1' search_by ='businesscategoryump,businessnameump,phone,zip,city,user_url' show_search_filter ='1' search_filter_items ='businesscategoryump' general_pagination_theme ='ihc-listing-users-pagination-1' pagination_pos ='top' ]");?>
 			
 			
-			
-
 		</div>
 	</main>
 </div>
 
-<script src=''. get_stylesheet_template_uri() . '/js/' .''></script>
+<script src="'<?= .get_stylesheet_directory_uri() . '/js/premium-members-club.js' . ?>'"></script>
 <?php get_footer(); ?>
